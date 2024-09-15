45578266ce9998f242ab6aafa2b041fb1d287c9a
thanhdt2106 <99344962+thanhdt2106@users.noreply.github.com>
2024-09-15 21:57:23 +0700

    commit thu2

--- src/main/java/com/example/demo/Dao/TheLoaiDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.TheLoaiEntity; 
-
-public interface TheLoaiDao extends JpaRepository<TheLoaiEntity, Integer>  {
-
-}
--- src/main/java/com/example/demo/Service/PalpayService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Service;
-
-import com.paypal.api.payments.Payment;
-import com.paypal.base.rest.PayPalRESTException;
-
-public interface PalpayService {
-	public Payment createPayment(Double total) throws PayPalRESTException;
-	public Payment executePayment(String paymentId, String payerId) throws PayPalRESTException;
-}
