d1bdf703e05dbdb76c0bbb81a330541e95ae183f
thanhdt2106 <99344962+thanhdt2106@users.noreply.github.com>
2024-10-06 21:50:59 +0700

    summit lần 4

--- RegisterData.java
+++ RegisterData.java
@@ -34,9 +34,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
 import ru.yandex.qatools.ashot.AShot;
 import ru.yandex.qatools.ashot.Screenshot;
